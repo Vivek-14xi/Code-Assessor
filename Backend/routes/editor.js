5e5d5c9851114686df2fe2332be481ad5dead4d6
Syed-Hamza13 <syed13hamza@gmail.com>
2025-09-27 06:46:46 +0530

    adding chatbot

--- Backend/routes/editor.js
+++ Backend/routes/editor.js
@@ -1,9 +1,17 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { handleChat } from "../chatcontroller/chatcontroller.js"
+
 const router = express.Router();
+
+// __dirname workaround for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../../Frontend/views/html/editor.html'));
 });
+router.post("/ask", handleChat);
 
-module.exports = router;
+export default router;
